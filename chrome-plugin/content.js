92f13ee92829f7f4da6ac626943e26725b2155ec
Zoltán Fraknói <zfr@phaseone.com>
2019-08-07 16:52:33 +0200

    styling improvements, canvas size

--- chrome-plugin/content.js
+++ chrome-plugin/content.js
@@ -28,27 +28,54 @@ function getTiff(url) {
     });
 }
 
-function displayTiffCanvas(canvas) {
-    document.body.append(canvas);
-    console.log(canvas);
+function displayTiffCanvas(domObj) {
+    var wrapper = document.createElement("div");
+    wrapper.setAttribute("class", "tiff-canvas-wrapper");
+
+    var menuBar = document.createElement("div")
+    menuBar.setAttribute("class", "menu-bar");
+    wrapper.appendChild(menuBar)
+
+    tifCanvas = domObj.tifCanvas;
+
+    wrapper.appendChild(tifCanvas);
+    domObj.embedObj.parentNode.parentNode.replaceChild(wrapper, domObj.embedObj.parentNode);
 }
 
 async function getTiffCanvases(domElements) {
     var tiffCanvases = []
 
-    for (let i = 0; i < embedDomElements.length; i++) {
-        var embedElement = embedDomElements[i];
+    for (let i = 0; i < domElements.length; i++) {
+        var embedElement = domElements[i];
 
         var isAlternatiffContent = embedElement.type == "application/x-alternatiff";
         if (isAlternatiffContent) {
+
             var canvas = await tiffToCanvas(embedElement.src);
-            tiffCanvases.push(canvas);
+
+            canvas.setAttribute("class", "tiff-canvas");
+            // canvas = setDomElementSize(canvas, dimensions.width + "px", "auto")
+            // canvas = setDomElementSize(canvas, "100%", "100%")
+
+            // canvas.setAttribute('style', 'width:' + (dimensions.width) +
+            // 'px;');
+
+            domObj = {
+                tifCanvas: canvas,
+                embedObj: embedElement
+            }
+            tiffCanvases.push(domObj);
         }
     }
 
     return tiffCanvases;
-
 }
+
+function setDomElementSize(element, width, height) {
+    element.setAttribute('style', 'width:' + (width) + '; height: ' + (height));
+    return element;
+}
+
 
 function clearDOM() {
     document.body.innerHTML = "";
@@ -57,11 +84,12 @@ function clearDOM() {
 async function displayCanvases(embedDomElements) {
     tiffCanvases = await getTiffCanvases(embedDomElements);
 
-    clearDOM();
+    // clearDOM();
 
     for (let i = 0; i < tiffCanvases.length; i++) {
-        var canvas = tiffCanvases[i];
-        displayTiffCanvas(canvas);
+        tiffCanvas = tiffCanvases[i];
+        var domObj = tiffCanvas;
+        displayTiffCanvas(domObj);
     }
 
 }
