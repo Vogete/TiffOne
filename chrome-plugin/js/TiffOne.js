b66e0219dbf7abf0975d73c60a303136a8c16e3c
Zoltán Fraknói <fraknoiz@gmail.com>
2019-08-20 21:09:33 +0200

    TiffOne class template started

---
+++ chrome-plugin/js/TiffOne.js
@@ -0,0 +1,43 @@
+/**
+ * A TiffOne object.
+ * Always call initialize first before you use
+ * the object (JavaScript Constructor can't be async)!
+ */
+class TiffOne {
+    constructor(tiffDomElement = null, targetDomElement = null) {
+        this._sourceTiffDomElement = tiffDomElement;
+        this._targetDomElement = targetDomElement;
+    }
+
+    /**
+     * (async) Initializes the object by loading the tiff file
+     * and creating all necessary components
+     */
+    async initialize() {
+
+    }
+
+    print() {
+
+    }
+
+    changePage() {
+
+    }
+
+    nextPage() {
+
+    }
+
+    previousPage(){
+
+    }
+
+    displayContent(){
+
+    }
+
+    generateTiffCanvas(){
+
+    }
+}
